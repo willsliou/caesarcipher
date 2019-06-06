ae2ae4f1225b341c82e5dbf1abafed4fa8b6db69
Wills Liou <willsliou@protonmail.com>
2019-06-05 21:22:23 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,54 @@
+/*
+  Title: Caesar Cipher
+  Description: This program takes in plain text and creates a cipher of that text. 
+  
+  How to use: Please enter plain type to encrypt: apples
+              The ciphered text is (with key shift: 1): bqqmft
+  
+  Review: Suppose letter; a = 0, b = 1, c = 2, d = 3, e = 4
+          key = 1 (aka. key shift)
+          ((int)letter + key) mod 26 ==> cast, return ciphered letter as number, cipherValue
+                                     ==> (char)cipherValue = '[some letter]'
+          Demo: 'a' in apples --> a = (0 + 1) mod 26
+          
+          
+  Notes: 
+  A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+  a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+  ASCII 'a'= 97, 'b'= 98, 'c'= 99, 'd'= 100
+  
+*/
+
+#include<stdio.h>// fget(), stdin macro
+#include<string.h>// stringlen()
+#include<stdlib.h>
+
+// Define Caesar Cipher Function
+void caesarCipher(char* plaintext, int key);
+
+int main(void){
+  int key = 1;
+  char plaintext[101];
+  
+  // Ask user 
+  print("Enter plain text to encrypt: ");
+  // Get input, store in plainText variable
+  fgets(plainText, sizeof(plainText, stdin); 
+  
+  printf("The ciphered text is: ");
+  
+  //system("pause") comment out if using macOS
+}
+
+  
+void caesarCipher(char* plaintext, int key){
+  int i=0;
+  int cypherValue;
+  char cypher;
+  
+  while(plainText[i]!= '\0' && strlen(plainText)-1 > i) {// '\0' is enter char
+    cypherValue = ((int)plainText[i] -97 + key) % 26 + 97 // ASCII - 'a'=97
+    cypher = (char)(cypherValue);
+    
+  }
+}
